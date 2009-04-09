b1c18ad8617d77e051148ff0e6613954bb56a230
Shawn Parker <shawn@topfroggraphics.com>
2009-04-09 01:34:33 -0600

    Adding new method of calling common commands so as to avoid code duplication. Adds a common WordPress ruby module definition for our common methods.

---
+++ Support/common.rb
@@ -0,0 +1,47 @@
+require ENV['TM_SUPPORT_PATH'] + '/lib/ui'
+require ENV['TM_SUPPORT_PATH'] + '/lib/exit_codes'
+
+module WordPress
+  
+  def self.bloginfo(get_bloginfo = false)
+    
+    bloginfo_command = get_bloginfo == true ? 'get_bloginfo' : 'bloginfo' 
+    
+    choices = [
+      { 'display' => 'admin_email' },
+      { 'display' => 'atom_url' },
+      { 'display' => 'charset' },
+      { 'display' => 'comments_atom_url' },
+      { 'display' => 'comments_rss2_url' },
+      { 'display' => 'description' },
+      { 'display' => 'home' },
+      { 'display' => 'html_type' },
+      { 'display' => 'language' },
+      { 'display' => 'name' },
+      { 'display' => 'pingback_url' },
+      { 'display' => 'rdf_url' },
+      { 'display' => 'rss2_url' },
+      { 'display' => 'rss_url' },
+      { 'display' => 'siteurl' },
+      { 'display' => 'stylesheet_directory' },
+      { 'display' => 'stylesheet_url' },
+      { 'display' => 'template_directory' },
+      { 'display' => 'template_url' },
+      { 'display' => 'text_direction' },
+      { 'display' => 'url' },
+      { 'display' => 'version' },
+      { 'display' => 'wpurl' }
+    ]
+    TextMate::UI.complete(choices)
+    
+    bloginfo = bloginfo_command + "('${0}');"
+    
+    scope = ENV['TM_SCOPE']
+    if scope.include? 'source.php.embedded.html'
+      puts bloginfo
+    else
+      puts '<?php ' + bloginfo + ' ?>'
+    end
+  end
+
+end
