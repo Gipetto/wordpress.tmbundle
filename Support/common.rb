014fa5320430d0482f0434e2a2e6b770166ae3f4
Shawn Parker <shawn@topfroggraphics.com>
2009-04-13 09:24:52 -0600

    Adding better command and action function definitions and more commands to autocomplete common items

--- Support/common.rb
+++ Support/common.rb
@@ -35,20 +35,116 @@ module WordPress
     ]
     TextMate::UI.complete(choices)
     
-    bloginfo = bloginfo_command + "('${0}');"
+    bloginfo = bloginfo_command + "('${1}');\$0"
     scope = ENV['TM_SCOPE']
     if scope.include? 'source.php.embedded.html'
-      puts bloginfo
+      TextMate.exit_insert_snippet(bloginfo)
     else
-      puts '<?php ' + bloginfo + ' ?>'
+      TextMate.exit_insert_snippet('<?php ' + bloginfo + ' ?>')
     end
   end
   
-  # add_action handler
-  #def self.add_action()
-  #  choices = OSX::PropertyList.load(File.read(ENV['TM_BUNDLE_SUPPORT'] + '/actions.plist'))
-  #  TextMate::UI.complete(choices)
-  #  TextMate::exit_insert_snippet("add_action('${0}');");
-  #end
-
+  # Wrapper for enqueue functions
+  def self.enqueue()
+    choices = [
+      { 'title' => 'script', 'func_to_call' => 'enqueue_script' },
+      { 'title' => 'script, from theme', 'func_to_call' => 'enqueue_from_theme' },
+      { 'title' => 'script, from plugin', 'func_to_call' => 'enqueue_from_plugin' },
+      { 'title' => 'style', 'func_to_call' => 'enqueue_style' }
+    ]
+    
+    t = TextMate::UI.menu(choices)
+    ret = eval t['func_to_call']
+    
+    scope = ENV['TM_SCOPE']
+    if scope.include? 'source.php.embedded.html'
+      TextMate.exit_insert_snippet(ret)
+    else
+      TextMate.exit_insert_snippet('<?php ' + ret + ' ?>')
+    end
+  end
+  
+  # enqueue a style
+  def self.enqueue_style()
+    style = "wp_enqueue_style('\${1:style_id}',get_bloginfo('template_directory').'\${2:/css/mystyle.css}',${3:array('\${4:string dependency}')},\${5:float version},'\${6:string media}');\$0"    
+    return style
+  end
+ 
+ # enqueue a script from the theme
+ def self.enqueue_from_theme()
+   script = "wp_enqueue_script('\${1:script_name}',get_bloginfo('template_directory').'\${2:/js/myscript.js}',${3:array('\${4:string dependency}')},\${5:float version});\$0"
+   return script
+ end
+  
+ # enqueue a script from a plugin
+ def self.enqueue_from_plugin()
+   script = "wp_enqueue_script('\${1:string script_id}','/index.php&\${2:my_action}=\${3:action_handler}',${4:array('\${5:string dependency}')},\${6:float version});\$0"
+   return script
+ end
+  
+ # enqueue a predefined script from WordPress
+ def self.enqueue_script()
+    choices = [
+      { 'display' => 'scriptaculous-root' },
+      { 'display' => 'scriptaculous-builder' },
+      { 'display' => 'scriptaculous-dragdrop' },
+      { 'display' => 'scriptaculous-effects' },
+      { 'display' => 'scriptaculous-slider' },
+      { 'display' => 'scriptaculous-sound' },
+      { 'display' => 'scriptaculous-controls' },
+      { 'display' => 'scriptaculous' },
+      { 'display' => 'cropper' },
+      { 'display' => 'swfupload' },
+      { 'display' => 'swfupload-degrade' },
+      { 'display' => 'swfupload-queue' },
+      { 'display' => 'swfupload-handlers' },
+      { 'display' => 'jquery' },
+      { 'display' => 'jquery-form' },
+      { 'display' => 'jquery-color' },
+      { 'display' => 'jquery-ui-core' },
+      { 'display' => 'jquery-ui-tabs' },
+      { 'display' => 'jquery-ui-sortable' },
+      { 'display' => 'interface' },
+      { 'display' => 'schedule' },
+      { 'display' => 'suggest' },
+      { 'display' => 'thickbox' },
+      { 'display' => 'sack' },
+      { 'display' => 'quicktags' },
+      { 'display' => 'colorpicker' },
+      { 'display' => 'tiny_mce' },
+      { 'display' => 'prototype' },
+      { 'display' => 'autosave' },
+      { 'display' => 'wp-ajax-response' },
+      { 'display' => 'wp-lists' },
+      { 'display' => 'common' },
+      { 'display' => 'editor' },
+      { 'display' => 'editor-functions' },
+      { 'display' => 'ajaxcat' },
+      { 'display' => 'admin-categories' },
+      { 'display' => 'admin-tags' },
+      { 'display' => 'admin-custom-fields' },
+      { 'display' => 'password-strength-meter' },
+      { 'display' => 'admin-comments' },
+      { 'display' => 'admin-users' },
+      { 'display' => 'admin-forms' },
+      { 'display' => 'xfn' },
+      { 'display' => 'upload' },
+      { 'display' => 'postbox' },
+      { 'display' => 'slug' },
+      { 'display' => 'post' },
+      { 'display' => 'page' },
+      { 'display' => 'link' },
+      { 'display' => 'comment' },
+      { 'display' => 'admin-gallery' },
+      { 'display' => 'media-upload' },
+      { 'display' => 'admin-widgets' },
+      { 'display' => 'word-count' },
+      { 'display' => 'wp-gears' },
+      { 'display' => 'theme-preview' }
+    ]
+    TextMate::UI.complete(choices)
+    
+    script = "wp_enqueue_script('\${1}');\$0" 
+    return script
+  end
 end
